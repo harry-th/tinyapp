59d7bc3ebcd181fb688b146b78b93daee22b717e
harry thomas <harryt1905@gmail.com>
2022-10-04 13:48:03 -0400

    added nodemon

--- express_server.js
+++ express_server.js
@@ -6,13 +6,44 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-app.set('view engine', "ejs");
+//stackoverflow random string function slightly altered
+const makeId = function() {
+  let result = '';
+  let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let i = 0;
+  while (i < 6) {
+    result += chars.charAt(Math.floor(Math.random() *
+chars.length));
+    i++;
+  }
+  return result;
+};
 
-app.get("/", (req, res) => {
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
-  res.render("urls_index.ejs", templateVars);
+  res.render("urls_index", templateVars);
 });
-
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.post('/urls',(req,res)=>{
+  let id = makeId();
+  urlDatabase[id] = req.body.longURL;
+  res.redirect(`/urls/${id}`);
+});
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  console.log(templateVars);
+  res.render("urls_show", templateVars);
+});
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
   
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
