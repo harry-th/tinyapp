499f3c60119c666bc1a1f4b74f087956124eef28
harry thomas <harryt1905@gmail.com>
2022-10-04 19:27:54 -0400

    login logout display and feature

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+let cookieParser = require('cookie-parser');
 const PORT = 8080; // default port 8080
+
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -24,7 +27,10 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"],
+  };
   res.render("urls_index", templateVars);
 });
 app.get("/urls/new", (req, res) => {
@@ -51,7 +57,14 @@ app.post("/urls/:id/delete", (req,res)=> {
   delete urlDatabase[req.params.id];
   res.redirect('/urls');
 });
-  
+app.post('/login',(req, res)=>{
+  res.cookie('username',req.body.username);
+  res.redirect('/urls');
+});
+app.post('/logout',(req, res)=>{
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
