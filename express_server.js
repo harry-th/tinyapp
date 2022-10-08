250ffa46e9874383c64cf141562a5f56eed0c324
harry thomas <harryt1905@gmail.com>
2022-10-08 00:54:05 -0400

    added screenshot

--- express_server.js
+++ express_server.js
@@ -13,14 +13,6 @@ app.use(cookieSession({
   keys: ['hello']
 }));
 const urlDatabase = {
-  b6UTxQ: {
-    longURL: "https://www.tsn.ca",
-    userID: "aJ48lW",
-  },
-  i3BoGr: {
-    longURL: "https://www.google.ca",
-    userID: "aJ48lW",
-  },
 };
 const users = {
   'aJ48lW': {
